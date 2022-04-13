8c0275c16e0d6e05351c9ebbd93914e8009eb26b
Mariia Telehonenko <telegonenko1@ukr.net>
2022-04-13 23:29:30 +0300

    change timer

---
+++ src/js/02-timer.js
@@ -0,0 +1,25 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+      console.log(selectedDates[0]);
+    //   console.log(selectedDates[0] - options.defaultDate.getTime() );
+      return selectedDates[0];
+  },
+};
+
+flatpickr('#datetime-picker', 
+ options );
+
+//  {enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     console.log(selectedDates[0]);
+//   },

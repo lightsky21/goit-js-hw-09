1c8e30de165a344c00fb1fcec8e2782b8eebb7c3
Mariia Telehonenko <telegonenko1@ukr.net>
2022-04-20 20:05:26 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,10 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 const buttonEl = document.querySelector('button');
 const formEl = document.querySelector('form');
-const amountInputEl = document.querySelector('input[name="amount"]');
-const stepInputEl = document.querySelector('input[name="step"]');
-const delayInputEl = document.querySelector('input[name="delay"]');
+// const amountInputEl = document.querySelector('input[name="amount"]');
+// const stepInputEl = document.querySelector('input[name="step"]');
+// const delayInputEl = document.querySelector('input[name="delay"]');
 
 
 // for (let index = 0; index < array.length; index++) {
@@ -14,11 +16,19 @@ formEl.addEventListener('submit', onFormElSubmit)
 
 function onFormElSubmit(e) {
   e.preventDefault();
+  const {
+    elements: { delay, step, amount }
+  } = e.currentTarget;
  
+  // const formElData = {
+  //   amount: JSON.parse(e.currentTarget.elements.amount.value),
+  //   step: JSON.parse(e.currentTarget.elements.step.value),
+  //   delay: JSON.parse(e.currentTarget.elements.delay.value),
+  // }
   const formElData = {
-    amount: JSON.parse(amountInputEl.value),
-    step: JSON.parse(stepInputEl.value),
-    delay: JSON.parse(delayInputEl.value),
+    amount: JSON.parse(amount.value),
+    step: JSON.parse(step.value),
+    delay: JSON.parse(delay.value),
   }
   
   CreateChainOfPromises(formElData);
@@ -32,10 +42,10 @@ function CreateChainOfPromises({amount, step, delay}) {
   for (let index = 1; index <= amount; index += 1) {
   createPromise(index, totalDelay)
   .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
   })
   .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
   });
     totalDelay += step;
 }
